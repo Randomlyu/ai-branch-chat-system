e0d80fd0e76883629c1fbccd7829ea6ca21e520f
Randomlyu <3424759804@qq.com>
2026-03-14 12:19:13 +0800

    删除一些无关组件

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -1,18 +1,8 @@
 <script setup lang="ts">
-import { RouterLink, RouterView } from 'vue-router'
+import {RouterView } from 'vue-router'
 </script>
 
 <template>
-  <header>
-    <div class="wrapper">
-      <nav>
-        <RouterLink to="/">Home</RouterLink>
-        <RouterLink to="/about">About</RouterLink>
-        <RouterLink to="/api-test">API测试</RouterLink>
-      </nav>
-    </div>
-  </header>
-
   <RouterView />
 </template>
 
